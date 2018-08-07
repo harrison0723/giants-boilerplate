0a2a8cfb4719766d8a4fa970b24afb28cf395368
Harrison Lo <harrison0723@gmail.com>
2018-08-07 09:01:13 +0800

    Add firebase/auth import

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/database'
+import 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBYZ_zURjItIRX9TDYdOCkwYW1T1R1ubI4",
